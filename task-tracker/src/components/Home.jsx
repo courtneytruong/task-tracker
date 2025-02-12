b00c0862f51159def263a1447c1b919fb11a7554
courtneytruong <courtneybtruong@gmail.com>
2025-02-12 15:34:48 -0500

    Home page finished

--- task-tracker/src/components/Home.jsx
+++ task-tracker/src/components/Home.jsx
@@ -1,15 +1,31 @@
-import { Container } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
+    <div className="body w-100 vh-100">
       <NavBar />
-      <div className="homeText w-100 vh-100">
-        <h1>Welcome to Task Tracker!</h1>
+      <Container className="homeText">
+        <Row>
+          <Col md={12}>
+            <h1>Welcome to Task Tracker!</h1>
+          </Col>
 
-        <p>The easy to use Task Tracking Application!</p>
-      </div>
+          <Col md={12}>
+            <h4>The easy to use Task Tracking Application!</h4>
+          </Col>
+
+          <Col md={12}>
+            <h6>Click the button below to get started:</h6>
+            <Link to="/TaskTracker">
+              <Button className="custom-submit-button">
+                Go to the Task Tracker
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
